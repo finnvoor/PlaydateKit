cc946948be1f377e5efa4b8763d4719bc88d7558
Finn Voorhees <finnvoorhees@gmail.com>
2024-03-16 17:24:30 +0000

    Remove old file

--- Sources/PlaydateKit/Macros.swift
+++
@@ -1,5 +0,0 @@
-@freestanding(declaration, names: named(game), named(eventHandler))
-public macro playdateEntry(_ game: PlaydateGame.Type) = #externalMacro(
-    module: "PlaydateKitMacros",
-    type: "PlaydateEntry"
-)
