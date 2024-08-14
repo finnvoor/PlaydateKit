0bc1397e3dd0f522c15fad3a920f3c7ecb106ad0
Finn Voorhees <finnvoorhees@gmail.com>
2024-08-14 22:47:16 +0100

    Add some Synth wrappers

--- Sources/PlaydateKit/Core/Sound.swift
+++ Sources/PlaydateKit/Core/Sound.swift
@@ -306,10 +306,76 @@ public enum Sound {
         private var samplePointer: OpaquePointer?
     }
 
+    public class Synth {
+        // MARK: Lifecycle
+
+        /// Creates a new synth object to play a waveform or wavetable. See ``Sound/Synth/setWaveform(_:)`` for waveform values.
+        public init() {
+            pointer = synth.newSynth.unsafelyUnwrapped().unsafelyUnwrapped
+        }
+
+        deinit {
+            synth.freeSynth.unsafelyUnwrapped(pointer)
+        }
+
+        // MARK: Public
+
+        public func setWaveform(_ waveform: SoundWaveform) {
+            synth.setWaveform.unsafelyUnwrapped(pointer, waveform)
+        }
+
+        /// Sets the attack time, in seconds.
+        public func setAttackTime(_ attackTime: Float) {
+            synth.setAttackTime.unsafelyUnwrapped(pointer, attackTime)
+        }
+
+        /// Sets the decay time, in seconds.
+        public func setDecayTime(_ decayTime: Float) {
+            synth.setDecayTime.unsafelyUnwrapped(pointer, decayTime)
+        }
+
+        /// Sets the sustain level, as a proportion of the total level (0.0 to 1.0).
+        public func setSustainLevel(_ sustainLevel: Float) {
+            synth.setSustainLevel.unsafelyUnwrapped(pointer, sustainLevel)
+        }
+
+        /// Sets the release time, in seconds.
+        public func setReleaseTime(_ releaseTime: Float) {
+            synth.setReleaseTime.unsafelyUnwrapped(pointer, releaseTime)
+        }
+
+        /// Plays a note with the current waveform or sample.
+        /// - Parameters:
+        ///   - frequency: The frequency of the note to play.
+        ///   - volume: 0 to 1, defaults to 1
+        ///   - length: In seconds. If omitted, note will play until you call noteOff()
+        ///   - when: Seconds since the sound engine started (see ``Sound/currentTime``). Defaults to the current time.
+        public func playNote(frequency: Float, volume: Float = 1, length: Float = -1, when: CUnsignedInt = 0) {
+            synth.playNote.unsafelyUnwrapped(pointer, frequency, volume, length, when)
+        }
+
+        /// Sends a note off event to the synth.
+        /// - Parameter when: The scheduled time to send a note off event. Defaults to immediately.
+        public func noteOff(when: CUnsignedInt = 0) {
+            synth.noteOff.unsafelyUnwrapped(pointer, when)
+        }
+
+        // MARK: Private
+
+        private let pointer: OpaquePointer
+    }
+
+    /// Returns the current time, in seconds, as measured by the audio device.
+    /// The audio device uses its own time base in order to provide accurate timing.
+    public static var currentTime: CUnsignedInt {
+        sound.getCurrentTime.unsafelyUnwrapped()
+    }
+
     // MARK: Private
 
     private static var sound: playdate_sound { Playdate.playdateAPI.sound.pointee }
     private static var sample: playdate_sound_sample { sound.sample.pointee }
+    private static var synth: playdate_sound_synth { sound.synth.pointee }
 
     private static var fileplayer: playdate_sound_fileplayer { sound.fileplayer.pointee }
     private static var sampleplayer: playdate_sound_sampleplayer { sound.sampleplayer.pointee }
