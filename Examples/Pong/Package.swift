c3feda63351372a3dcf2a7c10e0a6c8cfe500571
Finn Voorhees <finnvoorhees@gmail.com>
2026-01-30 18:32:29 +0000

    Add Playdate SDK include path to examples for Xcode module scanning (#156)

--- Examples/Pong/Package.swift
+++ Examples/Pong/Package.swift
@@ -8,6 +8,12 @@ import PackageDescription
 /// Xcode gains toolset.json support.
 let xcode = (Context.environment["XPC_SERVICE_NAME"]?.count ?? 0) > 2
 
+let playdateSDKPath: String = if let path = Context.environment["PLAYDATE_SDK_PATH"] {
+    path
+} else {
+    "\(Context.environment["HOME"]!)/Developer/PlaydateSDK/"
+}
+
 let package = Package(
     name: "Pong",
     platforms: [.macOS(.v14)],
@@ -28,7 +34,8 @@ let package = Package(
                     "-Xfrontend", "-disable-stack-protector",
                     "-Xfrontend", "-function-sections",
                     "-Xfrontend", "-gline-tables-only",
-                    "-Xcc", "-DTARGET_EXTENSION"
+                    "-Xcc", "-DTARGET_EXTENSION",
+                    "-Xcc", "-I", "-Xcc", "\(playdateSDKPath)/C_API",
                 ]),
             ],
         )
